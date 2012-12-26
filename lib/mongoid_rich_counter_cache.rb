3a205ec753f6e6126e014ee6c7dc38a2db58cd5b
helloqidi <helloqidi@gmail.com>
2012-12-26 17:55:33 +0800

    modify require

--- lib/mongoid_rich_counter_cache.rb
+++ lib/mongoid_rich_counter_cache.rb
@@ -1,2 +1,11 @@
-require "mongoid_rich_counter_cache/version"
-require "mongoid_rich_counter_cache/mongoid_rich_counter_cache"
+# encoding: utf-8
+
+#载入相对路径的文件
+def require_local(suffix)
+  require(File.expand_path(File.join(File.dirname(__FILE__), suffix)))
+end
+
+require_local('mongoid_rich_counter_cache/version')
+require_local('mongoid_rich_counter_cache/mongoid_rich_counter_cache')
+require_local('mongoid_rich_counter_cache/core')
+require_local('mongoid_rich_counter_cache/operation')
--- lib/mongoid_rich_counter_cache/core.rb
+++ lib/mongoid_rich_counter_cache/core.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-module Lakala
+module MongoidRichCounterCache
   # Core
   module ClassUtilMixin
 
